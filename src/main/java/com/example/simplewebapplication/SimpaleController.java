c3f9a9b98399510737d10072e2cc6bca1170c87f
NABIL <nabil.sarmouh@usmba.ac.ma>
2021-11-18 18:43:39 +0100

    Initial commit

---
+++ src/main/java/com/example/simplewebapplication/SimpaleController.java
@@ -0,0 +1,11 @@
+package com.example.simplewebapplication;
+import jdk.nashorn.internal.ir.RuntimeNode;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class SimpaleController {
+    @RequestMapping("/")
+    public  String  index(){
+    return "Hello World";
+    }
+}
