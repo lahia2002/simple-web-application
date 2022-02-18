5b1b8210cf26c1cfdcd235ef277ddd818cf6258a
Nabil <94637406+lahia2002@users.noreply.github.com>
2022-02-18 13:32:44 +0100

    dfg

--- src/main/java/com/example/simplewebapplication/SimpaleController.java
+++ src/main/java/com/example/simplewebapplication/SimpaleController.java
@@ -3,7 +3,7 @@ import jdk.nashorn.internal.ir.RuntimeNode;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-public class SimpaleController 
+public class SimpaleController {
     @RequestMapping("/")
     public  String  index(){
     return "Hello World";
